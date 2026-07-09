53e25b1ba0828bddec895aadd6f30dcccc02e970
Dileep Pal <131919319+paldileep@users.noreply.github.com>
2026-07-09 08:18:05 +0530

    Time: 0 ms (100%), Space: 58.1 MB (52.3%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,12 +4,12 @@
  */
 var removeDuplicates = function(nums) {
     
-    let slow = 0 
+    let slow = 1 
     for(let fast=1; fast<nums.length; fast++){ 
-        if(nums[slow]!==nums[fast]){ 
-            slow++
+        if(nums[slow-1]!==nums[fast]){ 
             nums[slow] = nums[fast]
+            slow++
         }
     }
-    return slow + 1
+    return slow 
 };
